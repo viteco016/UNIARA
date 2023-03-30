48bac08d7ad5907d28e9f6267fa6c7f2bf095385
Victor <Victor@DESKTOP-7NLMOHJ>
2023-03-30 19:05:11 -0300

    Exercicio 3

--- UniaraEstudos/src/aula02/Exercicio3.java
+++ UniaraEstudos/src/aula02/Exercicio3.java
@@ -1,5 +1,44 @@
 package aula02;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
 
+	public static void main(String[] args) {
+
+		Scanner tela = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro valor: ");
+		double n1 = tela.nextDouble();
+		
+		System.out.println("Digite o segundo valor: ");
+		double n2 = tela.nextDouble();
+		
+		System.out.println("Escolha o operador: +, -, * ou /");
+		char operador = tela.next().charAt(0);
+		
+		double resultado;
+		
+		switch (operador) {
+		case '+':
+			resultado = n1 + n2;
+			System.out.printf("%s + %s = %.2f", n1, n2, resultado);
+			break;
+		case '-':
+			resultado = n1 - n2;
+			System.out.printf("%s - %s = %.2f", n1, n2, resultado);
+			break;
+		case '*':
+			resultado = n1 * n2;
+			System.out.printf("%s * %s = %.2f", n1, n2, resultado);
+			break;
+		case '/':
+			resultado = n1 / n2;
+			System.out.printf("%s / %s = %.2f", n1, n2, resultado);
+			break;
+		default:
+			System.out.printf("ERRO!!!");
+		}
+		tela.close();
+	}
 }
